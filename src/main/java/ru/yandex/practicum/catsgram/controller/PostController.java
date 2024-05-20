a6ffb3a81d2b5c65052acb7571f6f09d76286b5a
Evgeniy Svistov <e.v.svistov@yandex.ru>
2024-05-20 14:15:21 +0500

    add UserController

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -5,9 +5,6 @@ import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
 
-import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.model.Post;
-
 import java.time.Instant;
 import java.util.Collection;
 import java.util.HashMap;
